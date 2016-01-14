5e929ec614bc67e63fac4ff132039000fd5db398
Tomio Ueda <tomio@jauntvr.com>
2016-01-14 13:33:45 -0800

    got pytest tests to work now with pytest cli

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,23 @@
-from django.core.urlresolvers import resolve
-from django.test import TestCase
-from lists.views import home_page
+# External Libraries
 import pytest
 
-# class HomePageTest(TestCase):
-#
-#     def test_root_url_resolves_to_home_page_view(self):
-#         found = resolve('/')
-#         self.assertEqual(found.func, home_page)
+# DJANGO LIBRARIES
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
 
-def test_home():
+from lists.views import home_page
+
+
+# TEST CASES ##########################################
+def test_root_url_resolves_to_home_page_view():
     found = resolve('/')
     assert found.func == home_page
 
-def test_home():
-    found = resolve('/main')
-    assert found.func == home_page
+def test_home_page_returns_correct_html():
+    request = HttpRequest()
+    response = home_page(request)
+
+    assert response.content.startswith(b'<html>') is True
+    assert (b'<title>To-Do lists</title>') in response.content
+    assert response.content.endswith(b'</html>') is True
--- superlists/README.md
+++
@@ -1,5 +0,0 @@
-# django-pytest
-=======
-Utilizing pytest with Django
-
-source ~/.virtualenvs/medusaqa_pytest/bin/activate
